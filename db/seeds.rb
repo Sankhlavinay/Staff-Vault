9803ce77c33b65c94abdeb558595965aaeea3970
Sankhlavinay <vinaysankhla4233@gmail.com>
2024-06-25 14:11:03 +0530

    First Commit

---
+++ db/seeds.rb
@@ -0,0 +1,47 @@
+
+employee = Employee.create(
+	 first_name: 'Vinay',
+	 last_name: 'Sankhla',
+	 personal_email: 'vinay@123',
+	 city: 'Indore',
+	 state: 'M.P.',
+	 country: 'India',
+	 pincode: '452001',
+	 address_line_1: 'vijay nagar indore'
+	)
+
+employee = Employee.create(
+	 first_name: 'Raaj',
+	 last_name: 'Sankhla',
+	 personal_email: 'raaj@123',
+	 city: 'Indore',
+	 state: 'M.P.',
+	 country: 'India',
+	 pincode: '452001',
+	 address_line_1: 'vijay nagar indore'
+	)
+
+employee = Employee.create(
+	 first_name: 'Tushar',
+	 last_name: 'Sankhla',
+	 personal_email: 'tushar@123',
+	 city: 'Indore',
+	 state: 'M.P.',
+	 country: 'India',
+	 pincode: '452001',
+	 address_line_1: 'vijay nagar indore'
+	)
+
+employee = Employee.create(
+	 first_name: 'Tushar',
+	 last_name: 'Bhiya',
+	 personal_email: 'tushar@123',
+	 city: 'Indore',
+	 state: 'M.P.',
+	 country: 'India',
+	 pincode: '452001',
+	 address_line_1: 'vijay nagar indore'
+	)
+ 
+# employee = Employee.new
+# employee.save
