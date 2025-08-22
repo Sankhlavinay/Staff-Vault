ae6bcd887de27f131f9566f0a1b8b84299fe4948
Sankhlavinay <vinaysankhla4233@gmail.com>
2025-08-23 02:10:36 +0530

    Rubocop Added

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,53 +1,52 @@
+# frozen_string_literal: true
+
 class DocumentsController < ApplicationController
-	before_action :set_document, only: [:edit, :update, :show, :destroy]
+  before_action :set_document, only: %i[edit update show destroy]
 
-	def index
-		@documents = Document.includes(:employee)
-	end	
+  def index
+    @documents = Document.includes(:employee)
+  end
 
-	def new
-		@document = Document.new
-	end
+  def new
+    @document = Document.new
+  end
 
-	def create
-		@document = Document.new(document_params)
-		if @document.save
-			redirect_to documents_path, notice: 'Document has been created successfull'
-		else
-			render :new
-		end
-	end
+  def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to documents_path, notice: 'Document has been created successfull'
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def edit; end
 
-	def update
-		if @document.update(document_params)
-			redirect_to documents_path, notice: 'Document has been updated successfully'
-		else
-			render :edit
-		end
-	end
+  def update
+    if @document.update(document_params)
+      redirect_to documents_path, notice: 'Document has been updated successfully'
+    else
+      render :edit
+    end
+  end
 
-	def show
-	end
+  def show; end
 
-	def destroy
-		if @document.destroy
-			redirect_to documents_path, notice: 'Document has been deleted successfully'
-		end
-	end
+  def destroy
+    return unless @document.destroy
 
+    redirect_to documents_path, notice: 'Document has been deleted successfully'
+  end
 
-	private
+  private
 
-	def document_params
-		params.require(:document).permit(:name, :doc_type, :employee_id, :image)
-	end
+  def document_params
+    params.require(:document).permit(:name, :doc_type, :employee_id, :image)
+  end
 
-	def set_document
-		@document = Document.find(params[:id])
-	rescue ActiveRecord::RecordNotFound => error
-		redirect_to documents_path, notice: error
-	end
+  def set_document
+    @document = Document.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to documents_path, notice: e
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,55 +1,55 @@
+# frozen_string_literal: true
+
 class EmployeesController < ApplicationController
-  
-	before_action :set_employee, only: [:edit, :update, :show, :destroy]
+  before_action :set_employee, only: %i[edit update show destroy]
 
-	def index
-		@q = Employee.ransack(params[:q])
+  def index
+    @q = Employee.ransack(params[:q])
     @employees = @q.result(distinct: true)
-	end	
+  end
 
-	def new
-		@employee = Employee.new
-	end
+  def new
+    @employee = Employee.new
+  end
 
-	def create
-		@employee = Employee.new(employee_params)
-		if @employee.save
-			redirect_to employees_path, notice: 'Employee has been created successfull'
-		else
-			render :new
-		end
-	end
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to employees_path, notice: 'Employee has been created successfull'
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def edit; end
 
-	def update
-		if @employee.update(employee_params)
-			redirect_to employees_path, notice: 'Employee has been updated successfully'
-		else
-			render :edit
-		end
-	end
+  def update
+    if @employee.update(employee_params)
+      redirect_to employees_path, notice: 'Employee has been updated successfully'
+    else
+      render :edit
+    end
+  end
 
-	def show
-	end
+  def show; end
 
-	def destroy
-		if @employee.destroy
-			redirect_to employees_path, notice: 'Employee has been deleted successfully'
-		end
-	end
+  def destroy
+    return unless @employee.destroy
 
+    redirect_to employees_path, notice: 'Employee has been deleted successfully'
+  end
 
-	private
+  private
 
-	def employee_params
-		params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country, :pincode, :address_line_1, :address_line_2, :date_of_birth, :date_of_joining, :job_title, :bio )
-	end
+  def employee_params
+    params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email, :city, :state, :country,
+                                     :pincode, :address_line_1, :address_line_2, :date_of_birth, :date_of_joining,
+                                     :job_title, :bio)
+  end
 
-	def set_employee
-		@employee = Employee.find(params[:id])
-	rescue ActiveRecord::RecordNotFound => error
-	redirect_to employees_path, notice: error
-	end
+  def set_employee
+    @employee = Employee.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to employees_path, notice: e
+  end
 end
